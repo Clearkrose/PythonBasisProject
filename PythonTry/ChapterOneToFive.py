0e19bc123137080cdaec4ff5bcd53d0e35da61ee
Clearkrose <3267141477@qq.com>
2023-04-08 18:54:02 +0800

    2023.4.3
    截止至第五章

--- PythonTry/ChapterOneToFive.py
+++ PythonTry/ChapterOneToFive.py
@@ -8,3 +8,52 @@ spam = ['cat', 'dog', 'bee', 'elephant']
 del spam[2]
 print(spam)
 
+cat = ['fat', 'black', 'loud']
+size, color, disposition = cat
+print(size)
+
+a = tuple(['cat', 'dog', 'elephant'])
+b = list(('cat', 'dog', 'elephant'))
+c = list('Hello')
+print(a, b, c)
+
+# 引用
+spam = 42
+cheese = spam
+spam = 100
+print(spam)
+print(cheese)
+
+spam = [0, 1, 2, 3, 4, 5]   # 指向
+cheese = spam
+cheese[1] = "hello!"
+print(spam)
+print(cheese)
+
+# Copy
+import copy
+spam = ['A', 'B', 'C', 'D']
+cheese = copy.copy(spam)
+cheese[1] = 42
+print(spam)
+print(cheese)
+
+picnicItems = {'apples':5, 'cop':2}
+print(picnicItems.get('apples', 0))
+print(picnicItems.get('egg', 0))
+
+d = picnicItems.setdefault('color', 'black')
+print(d)
+print(picnicItems)
+
+# 漂亮打印
+import pprint
+message = 'It was a bright cold day in April, and the clocks were striking thirteen'
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+pprint.pprint(count)
+print(pprint.pformat(count))    # 等价
